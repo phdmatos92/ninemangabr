24c5fb7cd8cd5c13922479e319643eb5eb2967c5
phdmatos92 <156485329+phdmatos92@users.noreply.github.com>
2024-01-13 08:26:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ class IndexController extends Controller {
                     "id": "last_release",
                     "url": "https://br.ninemanga.com/list/New-Update/"
                 },
-                {
-                    "title": "Manga Directory",
-                    "id": "manga_directory",
-                    "url": "https://br.ninemanga.com/category/index_{1}.html"
-                },
-                {
-                    "title": "Hot Manga",
-                    "id": "hot_manga",
-                    "url": "https://br.ninemanga.com/list/Hot-Book/"
-                },
-                {
-                    "title": "New Manga",
-                    "id": "new_manga",
-                    "url": "https://br.ninemanga.com/list/New-Book/"
-                }, 
             ]
         };
     }
